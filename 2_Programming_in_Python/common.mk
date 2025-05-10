26ce67da8aba5831cc3f733a3985f14386706500
Deiaa Eldin Hamad <dehamad@lab1r3s14.42abudhabi.ae>
2025-05-10 15:09:17 +0400

    "2nd Course - Programming in Python": added common.mk for fixed variables

---
+++ 2_Programming_in_Python/common.mk
@@ -0,0 +1,11 @@
+# Color definitions
+YELLOW   := \033[1;33m
+GREEN    := \033[1;32m
+BLUE     := \033[1;34m
+RED      := \033[1;31m
+RESET    := \033[0m
+NL       := \n
+
+# Docker configuration
+IMG_NAME := course_2_programming_in_python_img
+CON_NAME := course_2_programming_in_python_con 
