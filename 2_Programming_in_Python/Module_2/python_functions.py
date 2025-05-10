719c33b1654f2a6e5b4c4ceb83452ff3286a01b6
Deiaa Eldin Hamad <dehamad@lab1r3s14.42abudhabi.ae>
2025-05-10 15:50:15 +0400

    2nd Course - Programming in Python - Module 2: learned function

--- 2_Programming_in_Python/Module_2/python_functions.py
+++ 2_Programming_in_Python/Module_2/python_functions.py
@@ -7,8 +7,21 @@ print(sum(1, 2))
 
 print_separator()
 
-bill = 175.00
+# bill = 175.00
 
-tax_rate = 15
+# tax_rate = 15
 
-total_tax = (bill * tax_rate) / 100
+# total_tax = (bill * tax_rate) / 100
+
+# print("Total tax: ", total_tax)
+
+# print_separator()
+
+def calculate_tax(bill, tax_rate):
+    return round((bill * tax_rate) / 100, 2)
+
+print("Total tax: ", calculate_tax(175.00, 15))
+print("Total tax: ", calculate_tax(164.33, 22))
+
+print_separator()
+
