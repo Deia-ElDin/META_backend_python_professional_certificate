be9a9e4948b08db180f212022f9e415169ad84ba
Deiaa Eldin Hamad <dehamad@lab1r3s14.42abudhabi.ae>
2025-05-19 20:06:16 +0400

    2nd Course - Programming in Python - Module 3: completed Module 3

--- 2_Programming_in_Python/Module_3/14-python_practice1.py
+++ 2_Programming_in_Python/Module_3/14-python_practice1.py
@@ -113,3 +113,121 @@ class F(E,D,C):
 f = F()
 print(f.d())
 print(F.mro())
+
+"""
+F → E → all of E’s parents (in order) → D → all of D’s parents (in order) → C → object
+F → E → (B, C) → D (A, B) → C → object
+Remove Dublicates 
+F → E → B → C → D → A → object
+
+But oops! That violates the parent order of D(A, B) — it says A must come before B, 
+but here B came first (from E(B, C)).
+
+So Python fixes that by reordering it to keep parent orders correct 
+while still following the "first appearance wins" logic.
+
+F → E → B → C → D → A → object
+1 → 2 → 5 → 6 → 3 → 4 → object
+F → E → D → A → B → C → object
+
+What's the point of MRO?
+    MRO = Method Resolution Order
+
+    It's how Python decides which method to call when there's multiple inheritance.
+
+    Without MRO, Python wouldn’t know which method to run when multiple classes,
+    define the same function (.d(), .save(), .render()...)
+
+    It's used for:
+        1- Method overrides:
+            Shows which class’s method is called when multiple define the same one.
+
+        2- super() calls:
+            Determines the next class super() should go to in multi-inheritance.
+
+        3- Mixins:
+            Helps control the order and behavior of reusable mixin classes.
+"""
+
+print_separator()
+
+# Example 5
+# 1. Guess the output for the following block of code and try running the code once you have a solution in mind:
+
+class A:
+    def b(self):
+        return "Function inside A"
+
+class B:
+    pass
+
+class C:
+    def b(self):
+        return "Function inside C"
+
+class D(B, C, A):
+    pass
+
+class D(C):
+    pass
+
+"""
+    D -> C 
+    [<class '__main__.D'>, <class '__main__.C'>, <class 'object'>]
+"""
+
+d = D()
+print(d.b())
+print(D.mro())
+
+print_separator()
+
+# Example 6
+# 2. Guess the output for the following block of code and try running the code once you have a solution in mind:
+
+# class A:
+#     def c(self):
+#         return "Function inside A"
+
+# class B(A):
+#     def c(self):
+#         return "Function inside B"
+
+# class C(A,B):
+#     pass
+
+# class D(C):
+#     pass
+
+
+# d = D()
+# print(d.a)
+# print(D.mro())
+
+"""
+Error:
+TypeError: Cannot create a consistent method resolution order (MRO) for bases A, B
+"""
+
+print_separator()
+
+# Example 7
+# 3. Guess the output for the following block of code and try running the code once you have a solution in mind:
+
+# class A:
+#     pass
+
+# class B(A):
+#     pass
+
+# class C(B):
+#     pass
+
+
+# c = C()
+# print(c.a())
+
+"""
+Error:
+AttributeError: 'C' object has no attribute 'a'
+"""
