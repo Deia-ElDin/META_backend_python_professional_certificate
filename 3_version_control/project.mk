b09ebec65ea92b8d54ae32ddcd6826af5fbe114d
Deiaa Eldin Hamad <dehamad@lab1r3s14.42abudhabi.ae>
2025-05-23 23:01:28 +0400

    3rd Course - Version Control: Removed docker related variables and comments

--- 3_version_control/project.mk
+++ 3_version_control/project.mk
@@ -6,14 +6,3 @@ RED      := \033[1;31m
 RESET    := \033[0m
 NL       := \n
 
-# Course-specific Docker configuration
-COURSE 		:= version_control
-IMAGE 		:= $(COURSE)_img
-CONTAINER	:= $(COURSE)_container
-
-# Module paths
-M3 := Module_3/
-M4 := Module_4/
-
-check_container:
-	@docker ps | grep $(CONTAINER) > /dev/null || (printf "$(RED)Error: Container $(CONTAINER) is not running. Run 'make all' in the course directory first.$(RESET)$(NL)"; exit 1)
